ee531557ca4da029892a5ff3d931c831fba72759
Marc Hoersken <info@marc-hoersken.de>
2018-02-10 15:05:56 +0100

    Escape APACHE_ARGUMENTS variable in docker-entrypoint script

--- docker-entrypoint.d/01-source-envvars.sh
+++ docker-entrypoint.d/01-source-envvars.sh
@@ -7,7 +7,7 @@ if [ -f "/etc/apache2/envvars" ]; then
     echo "source /etc/apache2/envvars" >> /usr/local/sbin/apache2d
 fi
 
-echo "exec /usr/sbin/apache2 ${APACHE_ARGUMENTS}" >> /usr/local/sbin/apache2d
+echo "exec /usr/sbin/apache2 \${APACHE_ARGUMENTS}" >> /usr/local/sbin/apache2d
 
 chown --reference=/usr/sbin/apache2 /usr/local/sbin/apache2d
 chmod --reference=/usr/sbin/apache2 /usr/local/sbin/apache2d
