7f2970d117739747cb396e2526f3a37fc85c20df
Marc Hoersken <info@marc-hoersken.de>
2018-02-11 16:02:46 +0100

    Update website handling script to use bash instead of find

--- docker-entrypoint.d/02-enable-websites.sh
+++ docker-entrypoint.d/02-enable-websites.sh
@@ -1,8 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 
 if [ -d "${DOCKER_WEBSITES_DIR}" ]; then
-    /usr/bin/find "${DOCKER_WEBSITES_DIR}" -name '*.conf' -execdir /bin/ln -s "${DOCKER_WEBSITES_DIR}/{}" "/etc/apache2/sites-enabled/{}" \;
+    pushd "${DOCKER_WEBSITES_DIR}"
+    for website in *.conf; do
+        ln -s "${DOCKER_WEBSITES_DIR}/$website" "/etc/apache2/sites-available/$website"
+        ln -s "/etc/apache2/sites-available/$website" "/etc/apache2/sites-enabled/$website"
+    done
+    popd
 fi
 
 exit 0
